6d07f098333a7125a4bfcec55f5f5827d1940bbf
Tokyntsoa <37543784+Tokyntsoa@users.noreply.github.com>
2018-06-12 10:46:21 +0300

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,8 @@
 <?php
 	try{
-	$bdd = new PDO('mysql:host=localhost;dbname=mabase','root','');
+	$bdd = new PDO('mysql:host=localhost;dbname=mabase','root','root');
 	}catch (Exception $ex){
 		die('Erreur sur la base : '.$ex->getMessage());
 	}
 ?>
+
