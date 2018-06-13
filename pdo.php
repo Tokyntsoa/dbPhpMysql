0fd8d3690193dd7d6891ad2b6225939a8e890f6a
Tokyntsoa <37543784+Tokyntsoa@users.noreply.github.com>
2018-06-13 10:32:09 +0300

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,7 +1,7 @@
 <?php
 	try{
 	//$bdd = new PDO('mysql:host=localhost;dbname=mabase','root','root');
-	$bdd = new PDO('mysql:host=mysql-56-centos7-phpmysql.apps.192.168.96.100.nip.io;dbname=mabase','root','root');
+	$bdd = new PDO('mysql:host=mysql://mysql:3306/;dbname=mabase','root','root');
 	}catch (Exception $ex){
 		die('Erreur sur la base : '.$ex->getMessage());
 	}
