2bf35d248404a9c7586d9111e54cc1b0f9dcf5cb
Tokyntsoa <37543784+Tokyntsoa@users.noreply.github.com>
2018-08-10 11:29:54 +0300

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -7,11 +7,11 @@
 
 	$dsn = 'mysql:dbname='.$dbname.';host='.$dbhost.';port='.$dbport;
 	
-	echo " Host : ".$dbhost;
-	echo " Port : ".$dbport;
-	echo " User : ".$dbuser;
-	echo " Password : ".$dbpwd;
-	echo " Database : ".$dbname;
+	echo " Host : ".$dbhost."<br/>";
+	echo " Port : ".$dbport."<br/>";
+	echo " User : ".$dbuser."->".data['database-user']."<br/>";
+	echo " Password : ".$dbpwd."->".data['database-password']."-".data['database-root-password']."<br/>";
+	echo " Database : ".$dbname."->".data['database-name']."<br/>";
 	
 	echo "<br/><br/><br/>";
 
