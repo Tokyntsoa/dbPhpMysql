33d128abe30e47440b56011a5cbd66ea537ecbb9
Tokyntsoa <37543784+Tokyntsoa@users.noreply.github.com>
2018-08-10 11:05:08 +0300

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -3,9 +3,11 @@
 	$dbport = getenv("MYSQL_SERVICE_PORT");
 	$dbuser = getenv("MYSQL_USER");
 	$dbname = getenv("MYSQL_DATABASE");
-	$dbpwd = getenv("MYSQL_PASSWORD");
+	$dbpwd = getenv("MYSQL_ROOT_PASSWORD");
 
 	$dsn = 'mysql:dbname='.$dbname.';host='.$dbhost.';port='.$dbport;
+	
+	echo " Les valeurs sont : ".$dbhost."-".$dbport."-".$dbuser."-".$dbname."-".$dbpwd;
 	
 	try{		
 		$bdd = new PDO($dsn,$dbuser, $dbpwd);
