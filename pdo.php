98a61486c4e6907dcc9dac3d9757f15607d8c447
Tokyntsoa <37543784+Tokyntsoa@users.noreply.github.com>
2018-08-22 12:24:14 +0300

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -4,8 +4,6 @@
 	$dbuser = getenv("MYSQL_USER");
 	$dbname = getenv("MYSQL_DATABASE");
 	$dbpwd = getenv("MYSQL_ROOT_PASSWORD");
-
-	/*$dsn = 'mysql:dbname='.$dbname.';host='.$dbhost.';port='.$dbport;
 	
 	echo " Host : ".$dbhost."<br/>";
 	echo " Port : ".$dbport."<br/>";
@@ -15,10 +13,9 @@
 	
 	echo "<br/><br/><br/>";
 
-	*/
 	try{		
-		//$bdd = new PDO($dsn,$dbuser, $dbpwd);
-		$bdd = new PDO('mysql:host=172.30.190.14:3306;dbname=mabase','mysql','mysql');
+		$bdd = new PDO('mysql:host='.$dbhost.':'.$dbport.';dbname='.$dbname,$dbuser,$dbpwd);
+		//$bdd = new PDO('mysql:host=172.30.190.14:3306;dbname=mabase','mysql','mysql');
 	}catch (Exception $ex){
 		die('Erreur sur la base : '.$ex->getMessage());
 	}
